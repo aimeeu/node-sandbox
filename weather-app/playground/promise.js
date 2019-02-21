d2a6c5913d4fa293b2963d7afd2adcf50f8be867
Aimee Ukasick <aimeeu.opensource@gmail.com>
2019-02-21 14:12:34 -0600

    Add example of then/catch with Promise
    Signed-off-by: Aimee Ukasick <aimeeu.opensource@gmail.com>

--- weather-app/playground/promise.js
+++ weather-app/playground/promise.js
@@ -11,18 +11,48 @@ var asyncAdd = (a, b) => {
     });
 };
 
-asyncAdd(50,100).then((result) => {
+// asyncAdd(50,100).then((result) => {
+//     console.log('Result:', result);
+//     // chain promise
+//     return asyncAdd(result, '33');//should reject
+// }, (errorMessage) => {
+//     console.log('Error: ', errorMessage);
+// }).then((res) => {
+//     //this is for processing 'return asyncAdd'
+//     console.log('should be 183 ', res);
+//     }, (errorMessage) => {
+//     console.log('Error: ', errorMessage);
+// });
+
+//this causes the first promise to fail
+//promise assumes we have successfully handled the error
+// asyncAdd(50,'100').then((result) => {
+//     console.log('Result:', result);
+//     // chain promise
+//     return asyncAdd(result, 33);
+// }, (errorMessage) => {
+//     console.log('Error: ', errorMessage);
+// }).then((res) => {
+//     //this is for processing 'return asyncAdd'
+//     console.log('should be 183 ', res);
+// }, (errorMessage) => {
+//     console.log('Error: ', errorMessage);
+// });
+
+//adding error handling to catch unexpected errors
+asyncAdd(50,'100').then((result) => {
     console.log('Result:', result);
     // chain promise
     return asyncAdd(result, 33);
-}, (errorMessage) => {
-    console.log('Error: ', errorMessage);
 }).then((res) => {
     //this is for processing 'return asyncAdd'
     console.log('should be 183 ', res);
-    }, () => {
-    console.log('Error: ', errorMessage);
+}).catch((errorMessage) => {
+    //if any of the promise calls fail
+    console.log('Catch Error: ', errorMessage);
 });
+
+
 
 // promise is something that needs to be done; it is either resolved successfully or rejected with error
 // once a promise is resolved or rejected, execution returns
