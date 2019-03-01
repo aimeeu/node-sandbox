b35a35c2fd0b38a5e1b95749e513373654a4daf7
Aimee Ukasick <aimeeu.opensource@gmail.com>
2019-03-01 15:33:11 -0600

    Add hbs partials; refactor views
    add 404 handling
    
    Signed-off-by: Aimee Ukasick <aimeeu.opensource@gmail.com>

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,6 +2,7 @@
 const path = require('path');
 //third-party modules
 const express = require('express');
+const hbs = require('hbs');
 //app modules
 const geocode = require('./utils/geocode');
 const weather = require('./utils/weather.js');
@@ -9,16 +10,47 @@ const weather = require('./utils/weather.js');
 //init express
 const app = express();
 
+// template engine setup
+app.set('view engine', 'hbs');
 // customize to serve up static pages in public directory
-app.use(express.static(path.join(__dirname, '../public')));
+const publicPath = path.join(__dirname, '../public');
+app.use(express.static(publicPath));
+//hbs expects templates in a 'views' dir, so if you change name, tell app the location
+const viewsPath = path.join(__dirname, '../templates/views');
+app.set('views', viewsPath);
+// partials path
+const partialsPath = path.join(__dirname, '../templates/partials');
+hbs.registerPartials(partialsPath);
 
-// static routes served from public dir:
+
+// routes
 // app.com
 // app.com/help
 // app.com/about
-
-// dynamic routes:
 // app.com/weather
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'aimee'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About',
+        name: 'aimee'
+    });
+});
+
+app.get('/help', (req, res) => {
+    faqs = ['this the first item', 'second', 'third'];
+    res.render('help', {
+        title: 'Help',
+        FAQs: faqs,
+        name: 'aimee'
+    });
+});
 
 app.get('/weather', (req, res) => {
     let address = '12130 W Wren Court, Milwaukee, Wisconsin';
@@ -48,6 +80,15 @@ app.get('/weather', (req, res) => {
         });
 
     });
+});
+
+// must be listed last; this matches anything that isn't listed above
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: 'Page Not Found',
+        name: 'aimee',
+        errorMessage: 'Hmmmmm, something is rotten in Denmark...'
+    })
 });
 
 
