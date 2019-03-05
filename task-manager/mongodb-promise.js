cbe1c9ce7f42d74bd5a3ea6bc99b558ded088f0d
Aimee Ukasick <aimeeu.opensource@gmail.com>
2019-03-05 15:58:17 -0600

    Add delete
    Signed-off-by: Aimee Ukasick <aimeeu.opensource@gmail.com>

--- task-manager/mongodb-promise.js
+++ task-manager/mongodb-promise.js
@@ -14,32 +14,49 @@ MongoClient.connect(conUrl, {useNewUrlParser: true}, (error, client) => {
     console.log('connected correctly');
     const db = client.db(dbName);
 
-    // https://docs.mongodb.com/manual/reference/operator/update/
-    db.collection('users').updateOne( {
-        _id: new ObjectID("5c7eb26401ca8d724d0b3af0")
-    }, {
-        $set: {
-            name: 'Squiffy'
-        },
-        $inc: {
-            age: 1
-        }
-    }).then((result) => {
-        console.log(result.modifiedCount);
-    }).catch((error) => {
-        console.log(error);
-    })
+    // // https://docs.mongodb.com/manual/reference/operator/update/
+    // db.collection('users').updateOne( {
+    //     _id: new ObjectID("5c7eb26401ca8d724d0b3af0")
+    // }, {
+    //     $set: {
+    //         name: 'Squiffy'
+    //     },
+    //     $inc: {
+    //         age: 1
+    //     }
+    // }).then((result) => {
+    //     console.log(result.modifiedCount);
+    // }).catch((error) => {
+    //     console.log(error);
+    // })
+    //
+    //
+    // db.collection('tasks').updateMany({
+    //     completed: false
+    // }, {
+    //     $set: {
+    //         completed: true
+    //     }
+    // }).then((result) => {
+    //     console.log(result.modifiedCount)
+    // }).catch((error) => {
+    //     console.log(error)
+    // })
 
-
-    db.collection('tasks').updateMany({
-        completed: false
-    }, {
-        $set: {
-            completed: true
-        }
+    // remove users with age = 12
+    db.collection('users').deleteMany({
+        age: 12
     }).then((result) => {
-        console.log(result.modifiedCount)
+        console.log(result.deletedCount);
     }).catch((error) => {
         console.log(error)
-    })
+    });
+
+    db.collection('tasks').deleteOne( {
+        _id: new ObjectID("5c7ec9e971b9871d5d1aee4a")
+    }).then((result) => {
+        console.log(result.deletedCount);
+    }).catch((error) => {
+        console.log(error)
+    });
 });
